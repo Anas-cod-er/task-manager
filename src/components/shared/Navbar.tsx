98ca6d0b079e4c995341a854ea8765915ebf4d62
Joosep Alviste <joosep.alviste@gmail.com>
2026-05-06 06:24:48 +0800

    navbar degisn done

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// import { FaSearch } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-neutral-800 shadow-sm">
-            <div className="flex flex-row justify-between gap-2">
-                {/* <div className="flex flex-row">
-                    <div className="mt-10 mr-5"><FaSearch /></div> */}
-                <input type="text" placeholder="Search" className="text-gray-400 input input-bordered rounded-md w-24 md:w-auto"  />
-                {/* </div> */}
+        <div className="navbar bg-neutral-950 shadow-sm ">
+            <div className="flex flex-row justify-between gap-2 w-full">
+                <div className="flex flex-row p-2 gap-3 bg-slate-900 rounded-lg items-center text-gray-400">
+                    <FaSearch />
+                <input type="text" placeholder="Quick find task" className="outline-none focus:outline-none w-24 md:w-auto"  />
+                 </div>
                 
                 <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
