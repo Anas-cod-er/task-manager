6bf86c5a005f0d2207a3a51e5a243f33372a248a
Anas-cod-er <anasbinjaman100@gmail.com>
2026-05-06 06:50:53 +0800

    degsign navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -18,11 +18,6 @@ const Navbar = () => {
                         src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                     </div>
                 </div>
-                <ul
-                    tabIndex={-1}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li><a>Logout</a></li>
-                </ul>
                 </div>
             </div>
             </div>
